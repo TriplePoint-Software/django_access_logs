7aacd10c15b6197117d0f4b33bc90c6f6e21bc5b
Jai Vikram Singh Verma <jaivikram.verma@gmail.com>
2015-06-10 22:16:09 +0530

    fixed setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-access-logs',
+    name='django_access_logs',
     version='0.1',
     packages=['access_logs'],
     include_package_data=True,
-    license='Apache 2.0 License', 
+    license='Apache License, Version 2.0', 
     description='A simple module to record server access logs in DB and export them',
     long_description=README,
-    url='https://github.com/AssuraGroup/django-access-logs',
+    url='https://github.com/TriplePoint-Software/django_access_logs',
     author='Jai',
     author_email='jaivikram.verma@gmail.com',
     classifiers=[
@@ -31,7 +31,11 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires = [
-            'django-constance', 'ua-parser', 'django-import-export', 'python-dateutil',
-            'celery', 'django-celery', 
+        'django-constance>=0.6', 
+        'ua-parser>=0.3.6', 
+        'django-import-export>=0.2.7', 
+        'python-dateutil>=2.4.2',
+        'celery>=3.1.18', 
+        'django-celery>=3.1.16', 
         ],
 )
